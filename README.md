effe8c1d64bacb46cf54deccbb50e75fb479990c
Flo <flal@melix.net>
2018-06-25 22:45:29 +0200

    build system, and README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Un notebook pour apprendre [pandas](https://pandas.pydata.org)
+
+Le [notebook](https://github.com/flo-dhalluin/python-pandas-talk/blob/master/pandas-talk-run.ipynb)
+
+## Je veux jouer avec
+
+``` bash
+
+virtualenv --python=python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python data_generator.py
+jupyter notebook
+```
--- make_slides.sh
+++ make_slides.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # "execute" the notebook, and export to html (slides)
-jupyter nbconvert --to slides --execute pandas-talk.ipynb --ExecutePreprocessor.timeout=300
-# push slides to ftp ?
+jupyter nbconvert --to notebook --execute pandas-talk.ipynb --output pandas-talk-run.ipynb
+jupyter nbconvert --to slides pandas-talk-run.ipynb --ExecutePreprocessor.timeout=300
+
